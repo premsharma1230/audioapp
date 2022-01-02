36942a379f82bda3f218780034d4b71e8d40c9e6
premsharma1230 <60520468+premsharma1230@users.noreply.github.com>
2022-01-02 23:58:33 +0530

    code

---
+++ App.js
@@ -0,0 +1,56 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
+
+import {
+  Colors,
+  DebugInstructions,
+  Header,
+  LearnMoreLinks,
+  ReloadInstructions,
+} from 'react-native/Libraries/NewAppScreen';
+import AudioScreen from './Components/Audio';
+
+const App = () => {
+  return (
+    <View>
+      <AudioScreen />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  sectionContainer: {
+    marginTop: 32,
+    paddingHorizontal: 24,
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: '600',
+  },
+  sectionDescription: {
+    marginTop: 8,
+    fontSize: 18,
+    fontWeight: '400',
+  },
+  highlight: {
+    fontWeight: '700',
+  },
+});
+
+export default App;
---
+++ Components/Audio.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {Text, TextInput, View} from 'react-native';
+const AudioScreen = () => {
+  return (
+    <>
+      <View>
+        {/* <Text>Audio</Text> */}
+        <TextInput ></TextInput>
+      </View>
+    </>
+  );
+};
+
+export default AudioScreen;
